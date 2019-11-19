cb4f969b49565fac08ec99f029807e1785424d8d
PaulMansard <36075293+PaulMansard@users.noreply.github.com>
2019-11-19 16:45:07 +0100

    Add files via upload

---
+++ precache-manifest.12f96484cd61a97be6b492be3b133a51.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7095ac4ec0df0af1397",
+    "url": "/css/app.05b0fd4b.css"
+  },
+  {
+    "revision": "7e46e43043ddc8a4f8cf",
+    "url": "/css/chunk-vendors.528f884b.css"
+  },
+  {
+    "revision": "4a35b085012c3d11f80620ab42b78419",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be6707cb457508667797",
+    "url": "/js/about.35e252cc.js"
+  },
+  {
+    "revision": "e7095ac4ec0df0af1397",
+    "url": "/js/app.47011a78.js"
+  },
+  {
+    "revision": "7e46e43043ddc8a4f8cf",
+    "url": "/js/chunk-vendors.fcebf92b.js"
+  },
+  {
+    "revision": "a8c976ce1df196bdf9bbe5f3cfb86bee",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.db4c2b5195e0391b399375c31e45aebd.js"
+  "/precache-manifest.12f96484cd61a97be6b492be3b133a51.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "app-questionnaire-vue"});
